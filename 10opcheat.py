bfa5d88749620382d47ee47b08bd98a4c979832c
Lun4rIum <67378940+Lun4rIum@users.noreply.github.com>
2021-06-12 16:51:13 +0200

    Add files via upload

---
+++ 10opcheat.py
@@ -0,0 +1,55 @@
+import selenium
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.keys import Keys
+import time
+import pyperclip as pc
+import os
+
+user = os.getlogin()
+
+path = "/Users/"+str(user)+"/Downloads/chromedriver"
+options = webdriver.ChromeOptions()
+options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+driver = webdriver.Chrome(path, chrome_options=options)
+
+
+url = 'https://10fastfingers.com/typing-test/french'
+
+
+driver.get(url)
+time.sleep(5)
+
+ls = []
+
+try:
+    driver.find_element_by_id("CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll").click()
+    parser = argparse.ArgumentParser(description='[COOKIE] : Passed')
+    print("[COOKIE] : Passed")
+except:
+    print("[COOKIE] : No Cookie...")
+    pass       
+print("[ELEMENTS] : Loading Elements...")
+
+html = driver.page_source
+soup = BeautifulSoup(html, "lxml")
+  
+spans=soup.find('div', {'id': 'row1'}).text.split(' ')
+
+f = len(spans)
+
+i=0
+
+for x in range(f):
+
+    inputElement = driver.find_element_by_id("inputfield")
+    cop = pc.copy(spans[i])                   
+    inputElement.send_keys(Keys.COMMAND, 'v')
+    print('[Input] : sended :', "'",str(spans[i]),"'", ' | ', '[',i,']',"input sended")
+                    
+    inputElement.send_keys(Keys.SPACE)
+
+    i+=1
+
+
+
